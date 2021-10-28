c7dd7ef6216fd65891ba4a134fcc9abe134d75f4
alfiantyakencana2000 <alfiantyakencana2000@students.amikom.ac.id>
2021-10-28 10:01:23 +0700

    tugas2

---
+++ TugasLibraryUnitTest/UserTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TugasLibrary;
+using NUnit.Framework;
+
+
+namespace TugasLibraryUnitTest
+{
+    [TestFixture]
+    class UserTest
+    {
+        private User user;
+
+        [SetUp]
+        public void Init()
+        {
+            user = new User();
+        }
+
+        [Test]
+        public void IsValidUserTest()
+        {
+            Assert.IsTrue(user.IsValidUser("admin", "admin"));
+        }
+       
+    }
+}
